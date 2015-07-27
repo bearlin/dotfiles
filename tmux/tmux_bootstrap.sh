25b39efef42f33dd1aeca0768b42ac0eaa7ad3da
BearLin <bear.lin.001@gmail.com>
2015-07-28 00:19:54 +0800

    Update tmux/tmux_bootstrap.sh to be able to specify [remote.window.name] [remote.user.name] [remote.host.address]

--- tmux/tmux_bootstrap.sh
+++ tmux/tmux_bootstrap.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+usage() {
+  echo "Usage: " && \
+  echo "$0 [remote.window.name] [remote.user.name] [remote.host.address]" 
+}
+
+byebye() {
+  echo "ByeBye!"
+  echo
+  exit 0
+}
+
+if [ "$#" -lt 3 ]; then
+  usage
+  byebye "$@"
+fi
+
 # Creates a new tmux session, detaches it (so it doesn’t open in your current terminal) and names it hostlinst in this case.
 # -d  The new session is attached to the current terminal unless -d is given
 # -s  Specify the session name
@@ -8,9 +24,9 @@ tmux new-session -d -s hostlinst
 # The following set of new-window commands create five new windows and executes the command at the end.
 # -t hostlinst:1 tells tmux to "target" the session hostlinst and the window with the id of 1.
 # -n 'Server1' 'ssh xx@x.x.x.x' portion tells tmux to name the window Server1 and execute the 'ssh xx@x.x.x.x' command in it.
-tmux new-window -t hostlinst:1 -n 'Local1'
-tmux new-window -t hostlinst:2 -n 'Local2'
-tmux new-window -t hostlinst:3 -n 'Server1' 'ssh linst@127.0.0.1'
+tmux new-window -t hostlinst:1
+tmux new-window -t hostlinst:2
+tmux new-window -t hostlinst:3 -n "$1" "ssh $2@$3"
 
 # Tells tmux that you want to active window 1 of session 'hostlinst' 
 tmux select-window -t hostlinst:1
